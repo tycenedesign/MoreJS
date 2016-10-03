90b5a7ec0cc5973df5aaf03570d1e53fd94972de
Tycene Davis <tycenedavis@Labryinths-iMac.local>
2016-10-02 22:18:12 -0700

    I added an AddressBook js file

---
+++ AddressBook.js
@@ -0,0 +1,52 @@
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
+};
+
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888-8888",
+    email: "mary.johnson@example.com"
+};
+
+function add(firstName,lastName,email,phoneNumber){
+     contacts[contacts.length] = {
+    firstName: firstName,
+    lastName: lastName,
+     email: email,
+    phoneNumber: phoneNumber
+    };
+    
+};
+
+var contacts = [bob, mary,];
+
+
+function printPerson(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+
+function list() {
+	var contactsLength = contacts.length;
+	for (var i = 0; i < contactsLength; i++) {
+		printPerson(contacts[i]);
+	}
+};
+
+/*Create a search function
+then call it passing "Jones"*/
+var search = function(lastName){
+    var contactsLength = contacts.length;
+    for (var i = 0; i < contactsLength; i++){       
+         if(contacts[i].lastName === lastName){
+          printPerson(contacts[i]);
+         };
+    };
+};
+
+//search("Jones");
+add("Michelle", "Harrison","mh@gmail.com", "888-999-1212");
+list();
